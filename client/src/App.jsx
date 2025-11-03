62556d39974e49231a3717ce6c41ea287cc11d2c
Abhinav <abhinavacharya707@gmail.com>
2025-11-03 20:01:46 +0530

    added Pages Component and inside added Home with Grid Cards and Added Create Post Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from "./Navbar/Navbar";
+import Home from "./Pages/Home/Home";
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Home></Home>
     </div>
   );
 };
---
+++ client/src/Pages/Home/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Home.css";
+const Home = () => {
+  return (
+    <div className="Main">
+      <h1>WELCOME</h1>
+      <div className="card-grid">
+        <div className="card">
+          <h2>hi</h2>
+          <p>hi welcome there </p>
+        </div>
+        <div className="card">
+          <h2>hi</h2>
+          <p>hi welcome there </p>
+        </div>
+        <div className="card">
+          <h2>hi</h2>
+          <p>hi welcome there </p>
+        </div>
+        <div className="card">
+          <h2>hi</h2>
+          <p>hi welcome there </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
