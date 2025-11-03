fccade49ab7f5640a80103e7a84111df4ffc12ac
Abhinav <abhinavacharya707@gmail.com>
2025-11-03 21:10:31 +0530

    installed React RouteDom and added routes to Home Page and CreatePost Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Navbar from "./Navbar/Navbar";
 import Home from "./Pages/Home/Home";
+import CreatePost from "./Pages/CreatePost/CreatePost";
+import { Route, Routes } from "react-router-dom";
+
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/createPost" element={<CreatePost />}></Route>
+      </Routes>
     </div>
   );
 };
