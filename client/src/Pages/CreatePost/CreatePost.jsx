8182cd1f4048c94d49a9dfaf0bda4d800c1dfe82
Abhinav <abhinavacharya707@gmail.com>
2025-11-03 21:36:58 +0530

    added a Form in CreatePost Page containing inputs of Topic,Question and Answer

--- client/src/Pages/CreatePost/CreatePost.jsx
+++ client/src/Pages/CreatePost/CreatePost.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import "./CreatePost.css";
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  return (
+    <>
+      <div className="Post">
+        <h1>Create Post</h1>
+        <form className="form">
+          <input type="text" className="topic" placeholder="Topic" />
+          <input type="text" className="question" placeholder="Question" />
+          <textarea className="answer" rows="5" placeholder="Answer"></textarea>
+          <div>
+            <button type="submit" value="Send" className="submitWhite">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default CreatePost;
