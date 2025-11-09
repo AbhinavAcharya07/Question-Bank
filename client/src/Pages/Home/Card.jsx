a590e9c8aee9a159de5ef827e71317574a5f7a87
Abhinav <abhinavacharya707@gmail.com>
2025-11-09 21:05:39 +0530

    added card webkit-box clampings to question and answer section to limit their overflow hidden in 2 and 3 lines respectively

--- client/src/Pages/Home/Card.jsx
+++ client/src/Pages/Home/Card.jsx
@@ -3,9 +3,11 @@ import "./Card.css";
 const Card = ({ post }) => {
   return (
     <div className="card">
-      <h2>{post?.topic}</h2>
-      <p>{post?.question} </p>
-      <p>{post?.answer} </p>
+      <h2 className="card-topic">{post?.topic}</h2>
+      <div className="card-content">
+        <p className="card-question">{post?.question} </p>
+        <p className="card-answer">{post?.answer} </p>
+      </div>
     </div>
   );
 };
