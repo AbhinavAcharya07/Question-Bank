09baa541d177f0532a544ea1bf50ca8ab6a76212
Abhinav <abhinavacharya707@gmail.com>
2025-11-10 18:25:50 +0530

    added functionality with backend linkage to the delete button

--- client/src/Pages/SinglePost/SinglePost.jsx
+++ client/src/Pages/SinglePost/SinglePost.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./SinglePost.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 const SinglePost = () => {
   const { postId } = useParams();
   console.log(postId);
   const [post, setpost] = useState();
+  const navigate = useNavigate();
 
   const getPost = async () => {
     try {
@@ -21,6 +22,23 @@ const SinglePost = () => {
   useEffect(() => {
     getPost();
   }, []);
+  const deletePost = async () => {
+    try {
+      const response = await axios.delete(
+        "http://localhost:8001/api/deletepost?postId",
+        {
+          data: {
+            postId,
+          },
+        }
+      );
+      if (response.data.responseData) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="singlePost-Container">
       <div className="singlePost-Card">
@@ -28,7 +46,9 @@ const SinglePost = () => {
         <h2>{post?.question}</h2>
         <p>{post?.answer}</p>
         <div className="buttons">
-          <button id="deletebtn">Delete</button>
+          <button id="deletebtn" onClick={deletePost}>
+            Delete
+          </button>
           <button id="editbtn">Edit</button>
         </div>
       </div>
