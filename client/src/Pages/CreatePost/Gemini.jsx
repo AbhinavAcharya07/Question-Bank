da528b3b823d225d26fed37771c78b25e4e4cead
Abhinav <abhinavacharya707@gmail.com>
2025-11-11 23:31:50 +0530

    added search input with ask button

--- client/src/Pages/CreatePost/Gemini.jsx
+++ client/src/Pages/CreatePost/Gemini.jsx
@@ -23,9 +23,27 @@ const Gemini = () => {
         a dictionary of over 200 Latin words, combined with a handful of model
         sentence structures, to generate Lorem Ipsum which looks reasonable. The
         generated Lorem Ipsum is therefore always free from repetition, injected
-        humour, or non-characteristic words etc.
+        humour, or non-characteristic words etc. accident, sometimes on purpose
+        (injected humour and the like). There are many variations of passages of
+        Lorem Ipsum available, but the majority have suffered alteration in some
+        form, by injected humour, or randomised words which don't look even
+        slightly believable. If you are going to use a passage of Lorem Ipsum,
+        you need to be sure there isn't anything embarrassing hidden in the
+        (injected humour and the like). There are many variations of passages of
+        Lorem Ipsum available, but the majority have suffered alteration in some
+        form, by injected humour, or randomised words which don't look even
+        slightly believable. If you are going to use a passage of Lorem Ipsum,
+        you need to be sure there isn't anything embarrassing hidden in the
+        Lorem Ipsum available, but the majority have suffered alteration in some
+        form, by injected humour, or randomised words which don't look even
+        slightly believable. If you are going to use a passage of Lorem Ipsum,
+        you need to be sure there isn't anything embarrassing hidden in the
       </p>
-      <button>Ask</button>
+
+      <div className="InputWrapper">
+        <textarea name="" className="dynamicTextArea"></textarea>
+        <button className="send-btn">Ask</button>
+      </div>
     </div>
   );
 };
