ef69ad83815f83c68109f9a1c1621d72405fc850
Abhinav <abhinavacharya707@gmail.com>
2025-11-12 17:02:00 +0530

    added PayloadQuestion in the display by trimming it from input After Submit

--- client/src/Pages/CreatePost/Gemini.jsx
+++ client/src/Pages/CreatePost/Gemini.jsx
@@ -5,20 +5,32 @@ import { GoogleGenAI } from "@google/genai";
 
 const Gemini = () => {
   const [Askquestion, setAskquestion] = useState("");
-  const [Answer, setAnswer] = useState("");
+  const [Answer, setAnswer] = useState([]);
+  const [PayloadQn, setPayloadQn] = useState(null);
+  //   const PayloadQn
   const apiKey = "AIzaSyAjkxaO08jfNnTGNDr3CZ14Ce-tXqRgrhQ";
   const ai = new GoogleGenAI({
     apiKey: apiKey,
   });
   const SubmitHandler = async (e) => {
     e.preventDefault();
+    const trimmedQuestion = Askquestion.trim();
+
+    // 4. Input Validation (use .trim() before sending)
+    if (trimmedQuestion === "") {
+      return;
+    }
+    setPayloadQn(trimmedQuestion);
+    // 2. Instantly clear the input
+    setAskquestion("");
+    // 3. Clear the previous answer
+    setAnswer(null);
 
     try {
       const response = await ai.models.generateContent({
         model: "gemini-2.5-flash",
         contents: [Askquestion],
       });
-      setAskquestion("");
       setAnswer(response.text);
     } catch (error) {
       console.error("API Error:", error);
@@ -28,6 +40,9 @@ const Gemini = () => {
   return (
     <div className="Gemini">
       <div className="PayloadContainer">
+        <div className="PayloadQn">
+          <p>{PayloadQn}</p>
+        </div>
         <p>{Answer}</p>
       </div>
       <div className="InputWrapper">
