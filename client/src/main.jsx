70384defde2f10222ad9e0b35a11b524028120ca
Abhinav <abhinavacharya707@gmail.com>
2025-11-13 17:06:06 +0530

    Added contextApi For ModeSwitcher Feature and added Navbar ModeSwitcher feature

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-
+import { ModeProvoider } from "./contextProvider.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ModeProvoider>
+        <App />
+      </ModeProvoider>
     </BrowserRouter>
   </StrictMode>
 );
